e1fb946839e65ab5fa9b9b8df7d0a0dcddd63c47
HyNS00 <129006764+HyNS00@users.noreply.github.com>
2023-04-13 10:11:31 +0900

    [Bronze I] Title: 더하기 사이클, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,13 +1,16 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
 num = N
-count =0
-while True:
-    a = num %10
-    b = num // 10
-    c = (a+b) % 10
-    num = (a*10) +c
-    count +=1
+cnt =0
+while  True :
+    a = num % 10  # 26 % 10 = 6
+    b = num // 10 # 26 // 10  = 2
+    c = (a+b) % 10 # c =8
+
+    num = (a*10)+c   # 새로운 숫자는 마지막 a * 10  + c
+    cnt +=1 
     if num == N :
-        print(count)
+        print(cnt)
         break
-  
