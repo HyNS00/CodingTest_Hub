0b08ed079f93f41bbf841968c743f9bb3e963f8c
HyNS00 <129006764+HyNS00@users.noreply.github.com>
2023-04-16 21:18:12 +0900

    [Unrated] Title: 곱셈, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Unrated/1629. 곱셈/곱셈.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+A,B,C = map(int,input().split())
+def multi(a,n):
+    if n == 1:
+        return a % C
+    
+    else :
+        temp = multi(a, n//2)
+        if n % 2 == 0:
+            return (temp * temp) % C
+        
+        else :
+            return (temp * temp * a) % C
+        
+print(multi(A,B))
