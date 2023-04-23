dd49d978bc4ee63ad3e0e9085d7e69f357c92d85
HyNS00 <129006764+HyNS00@users.noreply.github.com>
2023-04-23 23:01:58 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 60 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,41 +1,32 @@
 import sys
-input =sys.stdin.readline
 sys.setrecursionlimit(10**9)
-N = int(input())
-N_list = list(map(int,input().split()))
+input = sys.stdin.readline
 
-add,sub,mul,div = map(int,input().split())
-min_value = 1e9
-max_value = -1e9
+n = int(input())
+data = list(map(int,input().split()))
+cal = list(map(int,input().split()))
+max_v = -1e9
+min_v = 1e9
+def dfs(i, plus,sub,mul,div,total):
+    global min_v, max_v
+    if i == n:
 
+        max_v= max(max_v,total)
+        min_v = min(min_v, total)
 
-def cal(i,arr):
-    global  add,sub,mul,div,min_value,max_value
-    if i == N:
-        max_value = max(max_value, arr)
-        min_value = min(min_value,arr)
+    else:
+        if plus:
+            dfs(i+1, plus -1, sub,mul,div,total + data[i])
 
-    else :
-        if add > 0 :
-            add -= 1
-            cal(i+1, arr+ N_list[i]) 
-            add += 1
+        if sub :
+            dfs(i+1, plus,sub-1,mul,div,total -data[i])
 
-        if sub > 0:
-            sub -= 1
-            cal(i+1,arr-N_list[i])
-            sub += 1
+        if mul:
+            dfs(i+1, plus,sub,mul-1,div,total * data[i])
 
-        if mul > 0:
-            mul -= 1
-            cal(i+1,arr * N_list[i])
-            mul += 1
+        if div :
+            dfs(i+1,plus,sub,mul,div-1,int(total / data[i]))
 
-        if div > 0:
-            div  -= 1
-            cal(i+1,int(arr/ N_list[i]))
-            div += 1
-
-cal(1, N_list[0])
-print(max_value)
-print(min_value)
+dfs(1, cal[0],cal[1],cal[2],cal[3],data[0])
+print(max_v)
+print(min_v)
