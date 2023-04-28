59029b4fb292a9546abc55a97f20d75b63323303
HyNS00 <129006764+HyNS00@users.noreply.github.com>
2023-04-28 20:40:09 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+
+d =[0] * 100
+
+
+def pibo(x):
+    if x == 1 or x == 2:
+        return 1
+    
+    if d[x] != 0:
+        return d[x]
+    
+
+    d[x] = pibo(x-1) + pibo(x-2)
+
+
+    return d[x]
+
+
+print(pibo(n))
