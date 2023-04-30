066ad30aec57b29367965117260e5ed0c00c722b
HyNS00 <129006764+HyNS00@users.noreply.github.com>
2023-04-30 22:47:49 +0900

    [Gold V] Title: 평범한 배낭, Time: 688 ms, Memory: 200704 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -7,17 +7,23 @@ for _ in range(N):
     W,V = map(int,input().split())
     x.append((W,V))
 
-graph =[[0] *(K+1) for _ in range(N+1)]
+memo = [[-1] *(K+1) for _ in range(N+1)]
 
-for i in range(1,N+1):
-    for j in range(1,K+1):
-        weight = x[i-1][0]
-        value = x[i-1][1]
+def knapsack(n, k):
+    if n == 0 or k == 0:
+        return 0
 
-        if j < weight:
-            graph[i][j] = graph[i-1][j]
+    if memo[n][k] != -1:
+        return memo[n][k]
 
-        else :
-            graph[i][j] = max(graph[i-1][j], value+graph[i-1][j-weight])
+    weight = x[n-1][0]
+    value = x[n-1][1]
 
-print(graph[-1][-1])
+    if k < weight:
+        memo[n][k] = knapsack(n-1, k)
+    else:
+        memo[n][k] = max(knapsack(n-1, k), value+knapsack(n-1, k-weight))
+
+    return memo[n][k]
+
+print(knapsack(N, K))
