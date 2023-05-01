c295563a2ea058db32b47bc887173a7bcef3500b
HyNS00 <129006764+HyNS00@users.noreply.github.com>
2023-05-01 16:40:50 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 704 ms, Memory: 117128 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
@@ -1,21 +1,29 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-matrix = [list(map(int, input().split())) for _ in range(N)]
-DP = [[0]*N for _ in range(N)]
+n = int(input()) # 행렬의 개수
+matrix = []  # n개의 행렬을 입력받아 matrix리스트에 저장한다.
+# 행렬 받기
+for _ in range(n):
+    r,c = map(int,input().split())
+    matrix.append((r,c))
+# 동적 dp테이블 초기화
+dp = [[0] *n for _ in range(n)]  #i,j구간의 최솟값을 저장하는 2차원 리스트
 
-# 분할된 그룹의 크기를 1부터 N-1까지 돎
-for size in range(1, N):
-	# 크기 size인 그룹의 모든 경우의 수 돎
-    for start in range(N - size):
-        end = start + size
-        
-        # 어떤 그룹의 최소 곱셈 횟수는 분할한 두 그룹의 최소 곱셈 횟수 + 각 그룹의 곱셈 다 끝나고 남은 행렬끼리의 곱셈 횟수
-        result = float("inf")
-        for cut in range(start, end):
-            result = min(result, DP[start][cut] + DP[cut+1][end] +
-                        matrix[start][0]*matrix[cut][1]*matrix[end][1])
-        DP[start][end] = result
-
-print(DP[0][-1])
+for i in range(1,n): # i는 구간의 길이를 의미
+    for j in range(n-i): # j는 구간의 시작 인덱스
+        '''
+        행렬의 크기는 n,연산을 수행하려면 최소 2개의 행렬이 필요하므로, j의 범위는 0부터 n-2까지, 인덱스j부터 n-1까지를 선택
+        심지어 j는 대각선에서 위쪽만 계산하면 되므로 범위가 0 ~ n-i
+        '''
+        x,y = j, i+j # x,y는 현재구간을 의미하는 변수
+        dp[x][y] = 2**32 # 최대값으로 초기화
+        for k in range(x,y):
+            dp[x][y] = min(dp[x][y], dp[x][k] + dp[k+1][y] +matrix[x][0]*matrix[k][1]*matrix[y][1])
+            '''
+            dp[x][k] x부터 k까지의 행렬을 곱한 최솟값
+            dp[k+1][y] = k+1부터 y까지의 행렬을 곱한 최솟값
+            matrix[x][0]*matrix[k][1]*matrix[y][1] : x부터 y까지의 행렬을 곱할 때의 곱셈 연산 횟수
+            dp[x][y]와 비교하여 더 작은 값을 dp[x][y]에 저장
+            '''
+print(dp[0][n-1])
