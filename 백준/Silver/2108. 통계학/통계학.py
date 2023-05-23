0f9611a460844c1094e08e1b4216a2b5a1316227
HyNS00 <129006764+HyNS00@users.noreply.github.com>
2023-05-23 10:57:44 +0900

    [Silver III] Title: 통계학, Time: 504 ms, Memory: 52640 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -36,9 +36,42 @@ mx_dic = []
 for i in dic:  # 이미 dic을 만들 때 sort한 list에서 차례대로 받았기 때문에 값들의 순서는 동일하다.
     if max_count == dic[i]:
         mx_dic.append(i)
+
+# print(mx_dic)
 if len(mx_dic) > 1: # 최빈값이 여러 개라면 
     print(mx_dic[1]) # 최빈값들 중 2번 째
 else :
     print(mx_dic[0])
 
 print(lst[-1] - lst[0])
+
+
+# 다르게 푸는 법
+'''
+import sys
+input = sys.stdin.readline
+sum =0
+n = int(input())
+lst = []
+dic ={}
+for _ in range(n):
+    a = int(input())
+    lst.append(a)
+    sum += a
+    if a in dic:
+        dic[n] += 1
+    
+    else :
+        dic[n] = 1
+
+
+dic_list = sorted(dic.items(),key = lambda x :(-x[1],x[0]))
+if n > 1 and dic_list[0][1] == dic_list[1][1]:
+    print(dic_list[1][0])
+
+else :
+    print(dic_list[1][0])
+
+# sorted를 통해서 먼저 values값들 즉 가장 많이 나온 값들에 대해 내림차순으로 정렬해주고
+# 그에 대한 최빈값들의 2번 째로 작은 값을 출력하기 위해서 동일한 최빈값에 대해서는 값들에 대해(= key) 정렬을 진행해준다.
+'''
