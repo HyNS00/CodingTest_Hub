57e90893872a84ce5a9b9ecf16d964d245c6e19f
HyNS00 <129006764+HyNS00@users.noreply.github.com>
2023-06-08 09:20:56 +0900

    Update ATM.py
    추가해설 및 느낀점

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -13,7 +13,6 @@ n = int(input())
 5 2
  인덱스랑 같이 받아볼까?
  min값을 해서 그리디로 해야할 것 같다
-cl
 '''
 result = 0
 people = list(map(int,input().split()))
@@ -21,5 +20,9 @@ people.sort()
 
 for i in range(1,n+1):
     result += sum(people[0:i])
+'''
+그냥 range(n)을 해버릴 경우 리스트의 인덱스에 의해 하나가 덜 출력되서 값이 이상하게 나온다
+그런 상황을 방지하고자 for문의 것을 1~n+1까지 설정하고 돌린다.
+'''
 
 print(result)
