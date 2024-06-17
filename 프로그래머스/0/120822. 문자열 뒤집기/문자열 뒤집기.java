cc343eecd4ed90e1285f63c93e3f9cb1ccb81938
HyNS00 <129006764+HyNS00@users.noreply.github.com>
2024-06-17 14:49:59 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.05 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,16 +1,8 @@
 import java.util.*;
 class Solution {
     public String solution(String my_string) {
-        char[] my_stringToChar = my_string.toCharArray();
-        List<Character> list = new ArrayList<>();
-        for(char c : my_stringToChar){
-            list.add(c);
-        }
-        Collections.reverse(list);
-        StringBuilder answer = new StringBuilder();
-        for(Character k : list){
-            answer.append(k);
-        }
-        return answer.toString();
+        StringBuilder answer = new StringBuilder(my_string);
+        
+        return answer.reverse().toString();
     }
 }
