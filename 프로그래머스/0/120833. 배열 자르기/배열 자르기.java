d0301288d73831a0c77b623467d3a84fa973dda3
HyNS00 <129006764+HyNS00@users.noreply.github.com>
2024-06-25 23:02:43 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -1,6 +1,10 @@
-import java.util.*;
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-       return Arrays.copyOfRange(numbers, num1, num2+1);
+        int capacity = num2 - num1 +1;
+        int[] answer = new int[capacity];
+        for(int i = 0; i < capacity; i++){
+            answer[i] = numbers[num1++];
+        }
+        return answer;
     }
 }
