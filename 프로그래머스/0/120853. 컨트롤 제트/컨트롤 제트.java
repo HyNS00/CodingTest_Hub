6c2fe23113fcf5fb017a9965e56b3f5e6effbf94
HyNS00 <129006764+HyNS00@users.noreply.github.com>
2024-07-13 23:27:29 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.16 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -5,9 +5,10 @@ class Solution {
         for(int i = 0; i < tmp.length; i++){
             if(tmp[i].equals("Z")){
                 sum -= Integer.parseInt(tmp[i-1]);
-                continue;
+                continue; // z가 되었을 경우 다음으로 넘어가는 걸 꼭 생각하자.
             }
             sum += Integer.parseInt(tmp[i]);
+            // else로 했었어도 나쁘지 않았을 것 같다.
         }
         return sum;
     }
