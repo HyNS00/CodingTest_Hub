d35d6f91d4ef0a574cfe8cb67e5d65d3b0afa6c6
HyNS00 <129006764+HyNS00@users.noreply.github.com>
2024-07-15 16:12:48 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.15 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -1,26 +1,16 @@
 class Solution {
     public int solution(String my_string) {
-        //[3,+,3,-,4,+,5,-,9]
-        //[0,1,2,3,4,5,6,7,8]
+        // 생각해보니까 간결화가 가능할거 같음
         String[] tmp = my_string.split(" ");
         int n = tmp.length;
-        int a = Integer.parseInt(tmp[0]);
-        int b = Integer.parseInt(tmp[2]);
-        int cal = 0;
-        if(tmp[1].equals("+")){
-            cal = a+b;
-        }else{
-            cal = a-b;
-        }
-         if(n > 3){
-            for(int i = 3; i < n; i+=2){
-                if(tmp[i].equals("+")){
-                    cal = cal + Integer.parseInt(tmp[i+1]);
-                }else{
-                    cal = cal - Integer.parseInt(tmp[i+1]);
-                }
+        int answer = Integer.parseInt(tmp[0]);
+        for(int i = 1; i < n; i+=2){
+            if(tmp[i].equals("+")){
+                answer += Integer.parseInt(tmp[i+1]);
+            }else{
+                answer -= Integer.parseInt(tmp[i+1]);
             }
         }
-        return cal;
+        return answer;
     }
 }
