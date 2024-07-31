cb1678c24f9228755d0125269f25bac011b52265
HyNS00 <129006764+HyNS00@users.noreply.github.com>
2024-07-31 19:26:36 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.04 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int num, int total) {
+        int[] answer = new int[num];
+        int a = (total/num) - ((num-1)/2);
+        for(int i = 0; i < num; i++){
+            answer[i] = a+i;
+        }
+        return answer;
+    }
+}
