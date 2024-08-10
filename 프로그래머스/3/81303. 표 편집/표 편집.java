f7edd49b94f4d36f75ba675280893f5db5de68c8
김현수 <129006764+HyNS00@users.noreply.github.com>
2024-08-11 00:05:02 +0900

    [level 3] Title: 표 편집, Time: 314.62 ms, Memory: 151 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,69 +1,54 @@
 import java.util.Stack;
-
-public class Solution {
+class Solution {
     public String solution(int n, int k, String[] cmd) {
-        class Node {
-            int value;
+        // 상대적인 위치를 생각해서 문제를 풀이
+        // 링크드 리스트 생각
+        class Node{
             Node prev;
             Node next;
-
-            public Node(int value) {
+            int value; // index역할
+            public Node(int value){
                 this.value = value;
             }
         }
-        // 노드들의 배열 (쉬운 접근을 위해)
         Node[] nodes = new Node[n];
-        
-        // 링크드 리스트 초기화
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++){
             nodes[i] = new Node(i);
-            if (i > 0) {
+            if(i > 0){
                 nodes[i].prev = nodes[i-1];
                 nodes[i-1].next = nodes[i];
             }
         }
-        
-        // 현재 선택된 노드
         Node current = nodes[k];
-        
-        // 삭제된 노드를 저장하는 스택
         Stack<Node> deleted = new Stack<>();
-        
-        // 명령어 실행
-        for (String c : cmd) {
-            if (c.startsWith("C")) {
+        for(String c : cmd){
+            if(c.startsWith("C")){
                 deleted.push(current);
-                
-                // 현재 노드 삭제
-                if (current.prev != null) current.prev.next = current.next;
-                if (current.next != null) current.next.prev = current.prev;
-                
-                // 다음 노드 선택
+                if(current.prev != null) current.prev.next = current.next;
+                if(current.next != null) current.next.prev = current.prev;
                 current = (current.next != null) ? current.next : current.prev;
-            } else if (c.startsWith("Z")) {
+            }else if(c.startsWith("Z")){
                 Node restore = deleted.pop();
-                
-                // 노드 복구
-                if (restore.prev != null) restore.prev.next = restore;
-                if (restore.next != null) restore.next.prev = restore;
-            } else {
+                // restore의 prev와 next는 그대로 유지하고 있다.
+                // 그자리에 복귀
+                if(restore.prev != null) restore.prev.next = restore;
+                if(restore.next != null) restore.next.prev = restore;
+            } else{
                 String[] s = c.split(" ");
-                int x = Integer.parseInt(s[1]);
-                
-                // 위아래로 이동
-                for (int i = 0; i < x; i++) {
-                    if (s[0].equals("U")) current = current.prev;
-                    else current = current.next;
+                int num = Integer.parseInt(s[1]);
+                for(int i = 0; i < num; i++){
+                    if(s[0].equals("U")){
+                        current = current.prev;
+                    }else{
+                        current = current.next;
+                    }
                 }
             }
         }
-        
-        // 결과 문자열 생성
-        StringBuilder answer = new StringBuilder("O".repeat(n));
-        while (!deleted.isEmpty()) {
-            answer.setCharAt(deleted.pop().value, 'X');
+        StringBuilder sb = new StringBuilder("O".repeat(n));
+        while(!deleted.isEmpty()){
+            sb.setCharAt(deleted.pop().value,'X');
         }
-        
-        return answer.toString();
+        return sb.toString();
     }
 }
