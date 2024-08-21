56f0b1d7682998a41bc2a7aac31f010835c32fc0
김현수 <129006764+HyNS00@users.noreply.github.com>
2024-08-21 17:52:40 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int num) {
+        long n = num;
+        for(int i = 0; i < 500; i++){
+            if(n == 1){
+                return i;
+            }
+            n = (n % 2 == 0) ? n/2 : n*3+1;
+            if(n > Integer.MAX_VALUE){
+                return -1;
+            }
+        }
+        return -1;
+    }
+}
