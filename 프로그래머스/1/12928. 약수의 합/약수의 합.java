db96fe774ddf2e36ef22af8c0e12ecdaaa25ca88
김현수 <129006764+HyNS00@users.noreply.github.com>
2024-08-21 13:46:14 +0900

    [level 1] Title: 약수의 합, Time: 0.09 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int i = 1;
+        int sum = 0;
+        while(i <= n){
+            if(n % i == 0){
+                sum+= i;
+            }
+            i++;
+        }
+        return sum;
+    }
+}
