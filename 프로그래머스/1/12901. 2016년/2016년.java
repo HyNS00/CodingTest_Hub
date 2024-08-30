d790b048af9de6198adfb58d976fc46ffe4756af
김현수 <129006764+HyNS00@users.noreply.github.com>
2024-08-31 00:21:34 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,17 @@
+class Solution {
+    
+    public String solution(int a, int b) {
+        // 1월 1일 
+        // a월 b일은 무슨 요일일까?
+        int day = 0;
+        int[] daysInMonth = {0,31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] week = {"SUN","MON","TUE","WED","THU","FRI","SAT"};
+        // 5 24 
+        for(int i = 1; i <= a-1; i++){
+            day += daysInMonth[i];
+        }
+        day += b-1;
+        // 3 + 6 = 9 % 7
+        return week[(day+5)% 7];
+    }
+}
