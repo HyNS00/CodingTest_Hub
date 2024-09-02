139daea98f6a4145dfbe34bbef42478bba558d2e
김현수 <129006764+HyNS00@users.noreply.github.com>
2024-09-02 15:13:25 +0900

    [level 1] Title: 덧칠하기, Time: 4.75 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,14 +1,17 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
+        int[] sections = new int[n];
+        for(int num : section){
+            sections[num-1] = 1;
+        }
         int cnt = 0;
-        int covered = 0;
-        for(int i : section){
-            if(i > covered){
+        for(int i = 0; i < n; i++){
+            if(sections[i] == 1){
                 cnt++;
-                covered = i + m-1;
-            }
-            if(covered > n){
-                covered = n;
+                for(int j = i; j < Math.min(i+m,n); j++){
+                    sections[j] = 0;
+                }
+                i = Math.min(i+m-1, n-1);
             }
         }
         return cnt;
