b372fb760bcfa2d3f60001a6bacbd27ce7dc9686
김현수 <129006764+HyNS00@users.noreply.github.com>
2024-09-11 13:51:56 +0900

    [level 1] Title: 대충 만든 자판, Time: 20.64 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,22 +1,21 @@
-import java.util.*;
 class Solution {
     public int[] solution(String[] keymap, String[] targets) {
-        int[] answer= new int[targets.length];
-        Map<Character, Integer> map = new HashMap<>();
-        for(String key : keymap){
-            for(int i = 0; i < key.length(); i++){
-                char c = key.charAt(i);
-                map.put(c, Math.min(i+1, map.getOrDefault(c, Integer.MAX_VALUE)));
-            }
-        }
-        for(int i = 0; i < targets.length; i++){
-            for(int j = 0; j < targets[i].length(); j++){
-                char c = targets[i].charAt(j);
-                if(map.containsKey(c)){
-                    answer[i] += map.get(c);
+        int[] answer = new int[targets.length];
+        
+        for(int i = 0; i< targets.length; i++){
+            String target = targets[i];
+            for(char c : target.toCharArray()){
+                int minidx = Integer.MAX_VALUE;
+                for(String key : keymap){
+                    int idx = key.indexOf(c);
+                    if(idx > -1 && minidx > idx){
+                        minidx = idx;
+                    }
+                }
+                if(answer[i] != -1 && minidx < Integer.MAX_VALUE){
+                    answer[i] += minidx+1;
                 }else{
                     answer[i] = -1;
-                    break;
                 }
             }
         }
