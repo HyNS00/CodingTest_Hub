e77858996b207775685f8ff043e7303094920e3e
김현수 <129006764+HyNS00@users.noreply.github.com>
2024-10-04 11:21:07 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 1.88 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,37 +1,65 @@
 import java.util.*;
 class Solution {
-    private HashMap<Character,Integer> map = new HashMap<>();
-    {
-        map.put('R',0);
-        map.put('T',0);
-        map.put('C',0);
-        map.put('F',0);
-        map.put('J',0);
-        map.put('M',0);
-        map.put('A',0);
-        map.put('N',0);
+    private class Category implements Comparable<Category> {
+        private String alpha;
+        private int value;
+        
+        private Category(String alpha, int value){
+            this.alpha = alpha;
+            this.value = value;
+        }
+        public int getValue(){
+            return value;
+        }
+        public String getAlpha(){
+            return alpha;
+        }
+        @Override
+        public int compareTo(Category other){
+            int num = Integer.compare(other.getValue(), this.getValue());
+            if(num != 0){
+                return num;
+            }else{
+                return this.getAlpha().compareTo(other.getAlpha());
+            }
+        }
+            
+        
     }
+    private Category[][] categories ={
+        new Category[]{new Category("R",0), new Category("T",0)},
+        new Category[]{new Category("C",0), new Category("F",0)},
+        new Category[]{new Category("J",0), new Category("M",0)},
+        new Category[]{new Category("A",0), new Category("N",0)}
+    };
     public String solution(String[] survey, int[] choices) {
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < survey.length; i++){
+        for(int i  = 0; i < survey.length; i++){
             int choice = choices[i];
             String tmp = survey[i];
             if(choice > 4){
-                addScore(tmp.charAt(1), choice);
+                addScore(tmp.charAt(1),choice);
             }else{
-                addScore(tmp.charAt(0),choice);
+                addScore(tmp.charAt(0), choice);
             }
         }
-        // abcdefghijklmnopqrstuvwxyz
-        // 삼항 연산자
-        sb.append(map.get('R') >= map.get('T') ? "R":"T");
-        sb.append(map.get('C') >= map.get('F') ? "C":"F");
-        sb.append(map.get('J') >= map.get('M') ? "J":"M");
-        sb.append(map.get('A') >= map.get('N') ? "A":"N");
+        for(Category[] cat : categories){
+            sb.append(choseOne(cat));
+        }
         return sb.toString();
     }
-    private void addScore(char c,int  choice){
-        int score = Math.abs(choice -4);
-        map.put(c, map.get(c)+score);
+    private void addScore(char c, int choice){
+        int score = Math.abs(choice - 4);
+        for(Category[] cat : categories){
+            for(Category ca : cat){
+                if(ca.getAlpha().equals(String.valueOf(c))){
+                    ca.value += score;
+                }
+            }
+        }
+    }
+    private String choseOne(Category[] cat){
+        Arrays.sort(cat);
+        return cat[0].getAlpha();
     }
 }
