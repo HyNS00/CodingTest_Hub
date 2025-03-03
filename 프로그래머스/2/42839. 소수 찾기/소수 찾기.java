eef51fafdba79370e22000d3cb00e493e9594de0
김현수 <129006764+HyNS00@users.noreply.github.com>
2025-03-03 11:29:38 +0900

    [level 2] Title: 소수 찾기, Time: 31.69 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.util.stream.*;
+class Solution {
+    private Set<Integer> set = new HashSet<>();
+    public int solution(String numbers) {
+        for(int i = 1; i <= numbers.length(); i++){
+            boolean[] visited = new boolean[numbers.length()];
+            permutation(visited, 0, i, "", numbers);
+        }
+        int answer = (int) set.stream().filter(x -> check(x))
+            .count();
+        return answer;
+    }
+    private void permutation(boolean[] visited, int depth, int checker, String curr, String numbers){
+        if(depth == checker){
+            set.add(Integer.parseInt(curr));
+            return;
+        }
+        
+        for(int i = 0; i < numbers.length(); i++){
+            if(!visited[i]){
+                visited[i] = true;
+                permutation(visited,depth+1,checker,curr + numbers.charAt(i), numbers);
+                visited[i] = false;
+            }
+        }
+    }
+    private boolean check(int num){
+        if(num <= 1) return false;
+        if(num == 2) return true;
+        if(num % 2 == 0) return false;
+        
+        for(int i = 3; i <= Math.sqrt(num); i += 2){
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
