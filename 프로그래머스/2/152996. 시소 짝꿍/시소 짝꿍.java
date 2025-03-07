57a78a690b6b915c5d3e88df9242790b0bec93ad
김현수 <129006764+HyNS00@users.noreply.github.com>
2025-03-07 18:26:21 +0900

    [level 2] Title: 시소 짝꿍, Time: 116.28 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -2,25 +2,37 @@ import java.util.*;
 class Solution {
     public long solution(int[] weights) {
         long answer = 0;
-
-		Map<Double, Integer> map = new HashMap<>();
+        Map<Double,Integer> map = new HashMap<>();
+        
         Arrays.sort(weights);
-		for(int weight : weights) {
-			answer += helper(weight, map);
-		}
-		return answer;
+        for(int w : weights){
+            double t = (double) w;
+            
+            if(map.containsKey(t)){
+                answer += map.get(t);
+            }
+            
+            if(map.containsKey(t * (1.0/2))){
+                answer += map.get(t* (1.0/2));
+            }
+            
+//             if(map.containsKey(t * (1.0/3))){
+//                 answer += map.get(t* (1.0/3));
+//             }
+            
+//             if(map.containsKey(t * (1.0/4))){
+//                 answer += map.get(t* (1.0/4));
+//             }
+            
+            if(map.containsKey(t * (2.0/3))){
+                answer += map.get(t* (2.0/3));
+            }
+            
+            if(map.containsKey(t * (3.0/4))){
+                answer += map.get(t* (3.0/4));
+            }
+            map.put(t, map.getOrDefault(t,0)+1);
+        }
+        return answer;
     }
-    public long helper(int w, Map<Double, Integer> map) {
-		long answer = 0;
-		double d1 = w*1.0;
-		double d2 = (w*2.0)/3.0;
-		double d3 = (w*1.0)/2.0;
-		double d4 = (w*3.0)/4.0;
-		if(map.containsKey(d1)) answer += map.get(d1);
-		if(map.containsKey(d2)) answer += map.get(d2);
-		if(map.containsKey(d3)) answer += map.get(d3);
-		if(map.containsKey(d4)) answer += map.get(d4);
-		map.put(w*1.0, map.getOrDefault(w*1.0, 0)+1);
-		return answer;
-	}
 }
