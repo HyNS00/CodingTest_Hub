313ad65029c64d3ee94eb47e69f27bbdf5ed1ae8
김현수 <129006764+HyNS00@users.noreply.github.com>
2025-04-14 08:55:44 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.11 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,65 +1,49 @@
-import java.util.*;
+
 class Solution {
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static boolean[][] visit;
+
+    static int[] answer;
+
+    public void dfs(int num, int x, int y, int count, String[] places){
+        if (count > 2) return;
+        if (count > 0 && count <= 2 && places[x].charAt(y) == 'P'){
+            //2칸 범위내에 다른 응시자가 있을 경우 거리두기 미준수로 0처리
+            answer[num] = 0;
+            return;
+        }
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            //배열 범위 밖으로 초과하는지 여부 검사, 파티션으로 분리되어 있는 경우 상관 없음.
+            if (nx >= 0 && nx < 5 && ny >= 0 && ny < 5 && places[nx].charAt(ny) != 'X') {
+                if (visit[nx][ny]) continue; //이미 방문한 곳일 경우 생략
+                visit[nx][ny] = true;
+                dfs(num, nx, ny, count + 1, places);
+                visit[nx][ny] = false;
+            }
+        }
+    }
+
     public int[] solution(String[][] places) {
-        int[] answer = new int[places.length];
-        int idx = 0;
-        for(String[] room : places){
-            if(checker(room)){
-                answer[idx++] = 1;
-            }else{
-                answer[idx++] = 0;
+        answer = new int[places.length];
+        for (int i = 0; i < places.length; i++) {
+            answer[i] = 1;
+        }
+
+        for (int i = 0; i < places.length; i++) {
+            visit = new boolean[5][5];
+            for (int j = 0; j < 5; j++) {
+                for (int k = 0; k < 5; k++) {
+                    if (places[i][j].charAt(k) == 'P'){
+                        visit[j][k] = true;
+                        dfs(i, j, k, 0, places[i]);
+                        // visit[j][k] = false;
+                    }
+                }
             }
         }
         return answer;
-    }
-    private boolean checker(String[] room){
-        List<int[]> list = new ArrayList<>();
-        for(int i = 0; i < 5; i++){
-            String tmp = room[i];
-            for(int j = 0; j < 5; j++){
-                if(tmp.charAt(j) == 'P'){
-                    list.add(new int[] {i,j});
-                }
-            }
-        }
-        if(distance(list,room)){
-            return true;
-        }
-        return false;
-    }
-    private boolean distance(List<int[]> list,String[] room){
-        int n = list.size();
-        for(int i = 0; i < n-1; i++){
-            int[] p1 = list.get(i);
-            for(int j = i+1; j < n; j++){
-                int[] p2 = list.get(j);
-                if(!mathatan(p1,p2,room)){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-    private boolean mathatan(int[] p1, int[] p2, String[] room){
-        int ax = p1[0];
-        int ay = p1[1];
-        
-        int bx = p2[0];
-        int by = p2[1];
-        
-        int m = Math.abs(ax - bx) + Math.abs(ay-by);
-        
-        if(m <= 1) return false;
-        if(m == 2){
-            if(ax == bx || ay == by){
-                int nx = (ax+bx) /2;
-                int ny = (ay+by) /2;
-                if(room[nx].charAt(ny) == 'X') return true;
-            }else{
-                if(room[ax].charAt(by) == 'X' && room[bx].charAt(ay) == 'X') return true;
-            }
-            return false;
-        }
-        return true;
     }
 }
