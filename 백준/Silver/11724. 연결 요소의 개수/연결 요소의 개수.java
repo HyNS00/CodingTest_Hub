18e27cd2a2cc9758329088d1dd4d53d629a23102
김현수 <129006764+HyNS00@users.noreply.github.com>
2025-09-01 18:08:29 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 672 ms, Memory: 155488 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,68 @@
+
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        List<List<Integer>> list = new ArrayList<>();
+
+        for(int i = 0; i <= N; i++){
+            list.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            list.get(a).add(b);
+            list.get(b).add(a);
+        }
+
+        boolean[] visited = new boolean[N+1];
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        int cnt = 0;
+
+        for(int i = 1; i < list.size(); i++){
+            if(!visited[i]){
+                cnt++;
+
+                stack.push(i);
+
+                while(!stack.isEmpty()){
+                    int node = stack.pop();
+
+                    if(!visited[node]) {
+                        visited[node] = true;
+
+                        for(int tmp : list.get(node)){
+                            if(!visited[tmp]) {
+
+                                stack.push(tmp);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        bw.write(String.valueOf(cnt));
+        bw.close();
+        br.close();
+
+    }
+
+}
