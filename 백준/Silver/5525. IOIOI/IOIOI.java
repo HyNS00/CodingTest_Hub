c86f769f642ebce4e9db56adbacddec5ecae2f2b
김현수 <129006764+HyNS00@users.noreply.github.com>
2025-09-08 12:20:43 +0900

    [Silver I] Title: IOIOI, Time: 240 ms, Memory: 22076 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -7,34 +7,36 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
+         long n = Long.parseLong(br.readLine());
+         long m = Long.parseLong(br.readLine());
 
         char[] s = br.readLine().toCharArray();
 
-        int result = checker(s, n, m);
+        long result = checker(s, n, m);
         bw.write(String.valueOf(result));
         bw.close();
         br.close();
     }
-    private static int checker(char[] target, int n, int m) {
-        int cnt = 0;
-        ArrayDeque<Character> stack  = new ArrayDeque<>();
-        int standard = 2 * n + 1;
-        for(int i = 0; i <= m - standard; i++) {
-            if(target[i] != 'I') continue;
-            stack.push(target[i]);
-            for(int j = i+1; j < i + standard; j++){
-                if(!stack.isEmpty() && stack.peek() != target[j]) {
-                    stack.push(target[j]);
-                }else {
-                    break;
-                }
+    private static long checker(char[] target, long n, long m) {
+        long cnt = 0;
+        long answer = 0;
+        for(int i = 0; i < m; i++){
+            if(i + 2 >= m) {
+                break;
             }
-            if(stack.size() == standard) cnt++;
-            stack.clear();
-        }
+            // 밀리는 조건 확인해보기
+            if(target[i] == 'I' && target[i+1] == 'O' && target[i+2] == 'I'){
+                cnt++;
+                i++;
 
-        return cnt;
+                if(cnt == n){
+                    answer++;
+                    cnt--;
+                }
+            }else {
+                cnt = 0;
+            }
+        }
+        return answer;
     }
 }
