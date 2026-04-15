27537be6f937acba283dcda8dfb0b108ad2f2376
김현수 <129006764+HyNS00@users.noreply.github.com>
2026-04-15 21:52:05 +0900

    [Gold I] Title: 트리의 순회, Time: 428 ms, Memory: 53848 KB -BaekjoonHub

--- 백준/Gold/2263. 트리의 순회/트리의 순회.java
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.java
@@ -5,47 +5,44 @@ import java.util.Arrays;
 
 public class Main {
     static StringBuilder sb;
+    static int[] inOrder;
+    static int[] preOrder;
+    static int[] inIdx;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // 후위 -> 노드가 마지막에 온다
-        // 중위 -> 가운데 있다.
         int n = Integer.parseInt(br.readLine());
-        // 인오더
-        int[] inOrder = Arrays.stream(br.readLine().split(" ")).mapToInt(num -> Integer.parseInt(num))
-                .toArray();
 
-        int[] postOrder = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        inOrder = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        preOrder = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        // 마지막 수가 노드
-        int[] idxIn = new int[n+1];
-
-        // 반복된다.
-        sb = new StringBuilder();
+        inIdx = new int[n + 1];
         for(int i = 0; i < n; i++) {
             int value = inOrder[i];
-            idxIn[value] = i;
-
+            inIdx[value] = i; // 해당 값의 index를 가져옴
         }
-        solve(0,n-1,0,n-1,idxIn,postOrder);
+
+        sb = new StringBuilder();
+        solve(0, n - 1, 0, n - 1);
         bw.write(sb.toString());
-        br.close();
         bw.close();
+        br.close();
     }
-    static void solve(int is,int il, int ps, int pl, int[] idxIn, int[] postOreder) {
+    static void solve(int is, int il, int ps, int pl) {
         if(is > il || ps > pl) {
             return;
         }
 
-        int root = postOreder[pl];
-        sb.append(root).append(" ");
+        int root = preOrder[pl];
+        int rootIdx = inIdx[root];
 
-        int rootIdx = idxIn[root];
+        sb.append(root).append(" ");
+        // 왼쪽 서브트리 == 오른쪽 서브 트리
         int leftSize = rootIdx - is;
 
-        solve(is, rootIdx - 1, ps, ps + leftSize - 1, idxIn, postOreder);
-        solve(rootIdx + 1, il, ps + leftSize, pl - 1,idxIn,postOreder);
-    }
+        solve(is, rootIdx -1, ps, ps + leftSize -1);
+        solve(rootIdx+1, il, ps + leftSize, pl -1);
 
+    }
 }
