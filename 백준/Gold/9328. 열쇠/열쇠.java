72ca75f03ca2d7df8f52b3c6cff605a52698b245
김현수 <129006764+HyNS00@users.noreply.github.com>
2026-04-16 14:08:28 +0900

    [Gold I] Title: 열쇠, Time: 196 ms, Memory: 19240 KB -BaekjoonHub

--- 백준/Gold/9328. 열쇠/열쇠.java
+++ 백준/Gold/9328. 열쇠/열쇠.java
@@ -1,16 +1,16 @@
 
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    static StringTokenizer st;
     static int N;
     static int M;
-    static char[][] map ;
-    static int[] dx = {0, 0, 1, -1};
+    static char[][] map;
+    static StringTokenizer st;
+    static Set<Character> keys;
+    static int[] dx = {0, 0, -1, 1};
     static int[] dy = {1, -1, 0, 0};
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -18,91 +18,114 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         for(int t = 0; t < T; t++) {
             st = new StringTokenizer(br.readLine());
+
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
 
             map = new char[N + 2][M + 2];
-            for (char[] row : map) {
+            // +2를 해야 감쌀 수 있다.
+
+            for(char[] row : map) {
                 Arrays.fill(row, '.');
             }
             for(int i = 0; i < N; i++) {
-                String tmp = br.readLine();
+                char[] tmp = br.readLine().toCharArray();
+
                 for(int j = 0; j < M; j++) {
-                    map[i + 1][j + 1] = tmp.charAt(j);
+                    map[i+1][j+1] = tmp[j];
                 }
             }
-            String k = br.readLine();
-            Set<Character> keys = new HashSet<>();
-            if (!k.equals("0")) {
-                for (char c : k.toCharArray()) {
+
+            String have = br.readLine();
+            keys = new HashSet<>();
+
+            if (!have.equals("0")) {
+                for(char c : have.toCharArray()) {
                     keys.add(c);
                 }
             }
-            // 26개의 알파벳 순서만큼 위치 추가
-            List<List<int[]>> list = new ArrayList<>();
-            for(int i = 0; i < 26; i++) {
-                list.add(new ArrayList<>());
-            }
 
-            sb.append(bfs(keys, list)).append("\n");
+            // 알파벳별 저장소 (26개) 만들기
+            List<List<int[]>> store = new ArrayList<>();
+            for(int i = 0; i < 26; i++) {
+                store.add(new ArrayList<>());
+            }
+            int result = bfs(store);
+
+            sb.append(result).append("\n");
+
+
         }
         bw.write(sb.toString());
         bw.close();
         br.close();
     }
-    static int bfs(Set<Character> keys, List<List<int[]>> list) {
+    static int bfs(List<List<int[]>> store) {
         int cnt = 0;
-        ArrayDeque<int[]> queue = new ArrayDeque<>();
         boolean[][] visited = new boolean[N + 2][M + 2];
-
-        queue.add(new int[]{0, 0});
         visited[0][0] = true;
 
+        ArrayDeque<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{0, 0});
+
         while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
+            int[] curr = queue.poll();
 
-            for (int dir = 0; dir < 4; dir++) {
-                int nx = cur[0] + dx[dir];
-                int ny = cur[1] + dy[dir];
+            int cx = curr[0];
+            int cy = curr[1];
 
-                if (nx < 0 || ny < 0 || nx >= N + 2 || ny >= M + 2) continue;
-                if (visited[nx][ny]) continue;
-                if (map[nx][ny] == '*') continue;
+            for(int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
 
-                char c = map[nx][ny];
+                if(nx < 0 || nx >= N+2 || ny < 0 || ny >= M+2) continue;
+                if(map[nx][ny] == '*') continue;
 
-                if ('A' <= c && c <= 'Z') {
-                    char key = Character.toLowerCase(c);
-                    if (!keys.contains(key)) {
-                        list.get(c - 'A').add(new int[]{nx, ny});
-                        continue;
-                    }
-                }
-
-                visited[nx][ny] = true;
-
-                if (c == '$') {
+                if(map[nx][ny] == '$' && !visited[nx][ny]) {
                     cnt++;
                     map[nx][ny] = '.';
-                } else if ('a' <= c && c <= 'z') {
-                    if (!keys.contains(c)) {
-                        keys.add(c);
-
-                        for (int[] pos : list.get(c - 'a')) {
-                            if (!visited[pos[0]][pos[1]]) {
-                                visited[pos[0]][pos[1]] = true;
-                                queue.add(new int[]{pos[0], pos[1]});
-                            }
-                        }
-                    }
-                    map[nx][ny] = '.';
+                    queue.add(new int[]{nx, ny});
+                    visited[nx][ny] = true;
                 }
 
-                queue.add(new int[]{nx, ny});
-            }
-        }
+                if (map[nx][ny] >= 'a' && map[nx][ny] <= 'z') {
+                    if (!keys.contains(map[nx][ny])) {
+                        keys.add(map[nx][ny]);
+                        int size = map[nx][ny] - 'a';
+                        List<int[]> doors = store.get(size);
+                        queue.add(new int[] {nx,ny});
+                        for(int[] tmp : doors) {
+                            visited[tmp[0]][tmp[1]] = true;
+                            map[tmp[0]][tmp[1]] = '.';
+                            queue.add(tmp);
+                        }
+                        visited[nx][ny] = true;
+                        map[nx][ny] = '.';
+                    }else {
+                        visited[nx][ny] = true;
+                        queue.add(new int[]{nx, ny});
+                        map[nx][ny] = '.';
+                    }
+                }
 
+                if (map[nx][ny] >= 'A' && map[nx][ny] <= 'Z') {
+                    if (!keys.contains(Character.toLowerCase(map[nx][ny]))) {
+                        int size = map[nx][ny] - 'A';
+                        store.get(size).add(new int[]{nx, ny});
+
+                    }else  {
+                        visited[nx][ny] = true;
+                        queue.add(new int[]{nx, ny});
+                        map[nx][ny] = '.';
+                    }
+                }
+                if (map[nx][ny] == '.' && !visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    queue.add(new int[]{nx, ny});
+                }
+            }
+
+        }
         return cnt;
     }
-
 }
